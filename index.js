6d72b58beacd7eea65bc69c0a80a80e9a261993a
qu1xs <kevinnealmurphy@gmail.com>
2020-08-16 18:03:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+function map(ary, cb){
+    return ary.map(i => cb(i))
+}
+
+function reduce(ary, cb, acu){
+    if (!!acu) {
+        return ary.reduce(cb, acu);
+      } else {
+        return ary.reduce(cb);
+      }
+}
